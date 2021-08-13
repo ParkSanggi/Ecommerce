854cd0c83a06dcb0708cff11c0907f2b76c66b05
ParkSanggi <roqkfwkehlwk@naver.com>
2021-08-13 23:53:50 +0900

    apply react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import Home from './component/home/home';
+import SellerJoin from './component/sellerJoin/sellerJoin';
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Router>
+          <Switch>
+            <Route path="/seller/join" component={SellerJoin} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
       </div>
     )
   }
 }
+
+
 
 export default App;
