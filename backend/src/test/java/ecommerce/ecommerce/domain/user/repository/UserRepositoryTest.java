9cfae674421465525e984f8c59f14157296ac3a5
ParkSanggi <roqkfwkehlwk@naver.com>
2021-08-25 23:46:44 +0900

    user table loginId 중복 불가 제약사항 테스트 추가

--- backend/src/test/java/ecommerce/ecommerce/domain/user/repository/UserRepositoryTest.java
+++ backend/src/test/java/ecommerce/ecommerce/domain/user/repository/UserRepositoryTest.java
@@ -1,12 +1,16 @@
 package ecommerce.ecommerce.domain.user.repository;
 
 import ecommerce.ecommerce.domain.user.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class UserRepositoryTest {
@@ -44,4 +48,22 @@ public class UserRepositoryTest {
         //then
         Assertions.assertFalse(result);
     }
+
+    @Test
+    void user_table_constraint_loginId가_중복_저장되려고_하면_런타임_예외() {
+        //given
+        String loginId = "test";
+        User user = User.builder()
+                        .loginId(loginId)
+                        .password("abcd").build();
+        User user2 = User.builder()
+                        .loginId(loginId)
+                        .password("1234").build();
+        userRepository.save(user);
+
+        //when
+        SQLIntegrityConstraintViolationException result =
+                Assertions.assertThrows(
+                        SQLIntegrityConstraintViolationException.class, () -> userRepository.save(user2));
+   }
 }
