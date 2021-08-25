5b7f1f5c5d9cd992976e00cb663e78580c340124
ParkSanggi <roqkfwkehlwk@naver.com>
2021-08-25 23:44:45 +0900

    save된 객체를 테스트하기 위한 인터페이스 추가

--- backend/src/main/java/ecommerce/ecommerce/domain/user/repository/UserRepository.java
+++ backend/src/main/java/ecommerce/ecommerce/domain/user/repository/UserRepository.java
@@ -3,6 +3,9 @@ package ecommerce.ecommerce.domain.user.repository;
 import ecommerce.ecommerce.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByLoginId(String loginId);
+    Optional<User> findByLoginId(String loginId);
 }
